80bc8b7cbd5b8aff27c71008f64893a572a55e46
Omar Mohamed <99252081+OmarMohammedd@users.noreply.github.com>
2023-09-12 20:22:07 +0300

    first commit

---
+++ js.js
@@ -0,0 +1,12 @@
+let container = document.querySelector(".container");
+let img = document.querySelector(".image img");
+
+
+function phone(o) {
+    img.src = o ;
+}
+
+
+function colors(o) {
+    container.style.background = o ;
+}
